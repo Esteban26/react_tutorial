cca06e3168e15ffd5dda1763f0812b96614ec21e
Esteban26 <ing.esteban.hdz.dav@gmail.com>
2021-02-17 23:39:14 -0600

    Interpolate JSX

--- 04-create-hello-world/src/App.js
+++ 04-create-hello-world/src/App.js
@@ -6,13 +6,22 @@ import Header from './components/Header.jsx'
 function App() {
   const name = 'Esteban'
   const age = '29'
+  const login = false
 
-  return (
-    <>
-      <Header />
-      <h1>My name is {name} and I'm {age} old.</h1>
-    </>
-  );
+  if (login)
+    return (
+      <>
+        <Header />
+        <h1>My name is {name} and I'm {age} old.</h1>
+      </>
+    );
+  else
+    return (
+        <>
+          <Header />
+          <h1>Usuario anónimo</h1>
+        </>
+    )
 }
 
 export default App;
