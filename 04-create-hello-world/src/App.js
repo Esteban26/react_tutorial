d8a5487b163c82a41ba6ceade863266b8a5e7ed6
Esteban26 <ing.esteban.hdz.dav@gmail.com>
2021-02-23 21:51:43 -0600

    1rst practice

--- 04-create-hello-world/src/App.js
+++ 04-create-hello-world/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 //Components 
-import Header from './components/Header.jsx'
+import Header from './components/Header'
 import Person from './components/Person'
+import Card from './components/Card'
 // third party libraries.
 import { nanoid as getNanoId } from 'nanoid'
 
@@ -11,6 +12,38 @@ function App() {
   const login = true
   const numbers = [1, 2, 3, 4, 5]
   const familyMembers = ['Siman', 'Moshi-oshi', 'Sarita', 'Deivi']
+  const cards = [
+    {
+      id: getNanoId(),
+      title: "Card 1",
+      img: '/assets/wolf.jpg',
+      author: 'Dorian',
+      date: new Date().toLocaleDateString(),
+      tags: ['animal', 'wolf', 'snow'],
+      views: 23,
+      description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
+    },
+    {
+      id: getNanoId(),
+      title: "Card 2",
+      img: '/assets/wolf.jpg',
+      author: 'Dorian',
+      date: new Date().toLocaleDateString(),
+      tags: ['animal', 'wolf', 'snow'],
+      views: 23,
+      description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
+    },
+    {
+      id: getNanoId(),
+      title: "Card 3",
+      img: '/assets/wolf.jpg',
+      author: 'Dorian',
+      date: new Date().toLocaleDateString(),
+      tags: ['animal', 'wolf', 'snow'],
+      views: 23,
+      description: "A beautiful wolf in snow. Lorem ipsum dolor sit amet consectetur adipiscing elit integer, dictumst nibh sociosqu torquent tempor blandit neque habitasse, sodales mi nisl praesent rhoncus urna pharetra. Leo integer sem volutpat feugiat fusce luctus, arcu laoreet urna mauris cum mus platea, pulvinar magna mattis auctor penatibus. Laoreet cubilia duis luctus vivamus nunc pulvinar ligula porta vehicula hendrerit convallis eleifend sed, auctor proin dignissim integer vel tristique rutrum rhoncus purus ultricies curabitur."
+    }
+  ]
 
   return (login) &&
     (
@@ -49,7 +82,27 @@ function App() {
         >
           This is the children.
         </Person>
+        <hr />
+        { /* Reusable component */}
+        <div className="cards">
+          {
+            cards.map(card => (
+              <Card
+                author={card.author}
+                date={card.date}
+                img={card.img}
+                key={card.id}
+                tags={card.tags}
+                title={card.title}
+                views={card.views}
+              >
+                {card.description}
+              </Card>
+            ))
+          }
+        </div>
       </>
+
     )
 }
 
